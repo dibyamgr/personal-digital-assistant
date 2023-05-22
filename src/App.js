d34a1b60584141a7409b7b778d6d18df3858c61f
dibyamgr <dibyamagar5@gmail.com>
2023-05-22 22:23:58 +0545

    PDA Design

--- src/App.js
+++ src/App.js
@@ -7,18 +7,23 @@ import SiriImage from "./images/siri.png";
 function App() {
   return (
     <>
-      <div>Personal Digital Assistant</div>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistant</p>
+        </div>
+      </section>
+
       <div className="container">
         <section className="section"></section>
         <div className="columns">
           <div className="column is-4">
-            <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+            <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} description="Alexa was created by Amazon and helps you buy things." />
           </div>
           <div className="column is-4">
-            <ProfileCard title="Cortana" handle="@cortana99" image={CortanaImage} />
+            <ProfileCard title="Cortana" handle="@cortana99" image={CortanaImage} description="Cortana was created by Microsoft and helps you buy things." />
           </div>
           <div className="column is-4">
-            <ProfileCard title="Siri" handle="@siri99" image={SiriImage} />
+            <ProfileCard title="Siri" handle="@siri99" image={SiriImage} description="Siri was created by Apple and being phased out." />
           </div>
         </div>
       </div>
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,4 +1,4 @@
-function ProfileCard({ title, handle, image }) {
+function ProfileCard({ title, handle, image, description }) {
   return (
     <div className="card">
       <div className="card-image">
@@ -7,8 +7,11 @@ function ProfileCard({ title, handle, image }) {
         </figure>
       </div>
       <div className="card-content">
-        <p className="title is-4">Title is {title}</p>
-        <p className="subtitle is-6">Handle is {handle}</p>
+        <div className="media-content">
+          <p className="title is-4">Title is {title}</p>
+          <p className="subtitle is-6">Handle is {handle}</p>
+        </div>
+        <div className="content">{description}</div>
       </div>
     </div>
   );
