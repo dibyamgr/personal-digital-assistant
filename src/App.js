a1da3e064680de19edc2966d14a6db893c8011fb
dibyamgr <dibyamagar5@gmail.com>
2023-05-22 21:44:34 +0545

    Profile Card Component Setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 function App() {
   return (
     <>
       <div>Personal Digital Assistant</div>
-      <ProfileCard />
-      <ProfileCard />
-      <ProfileCard />
+      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} />
+      <ProfileCard title="Cortana" handle="@cortana99" image={CortanaImage} />
+      <ProfileCard title="Siri" handle="@siri99" image={SiriImage} />
     </>
   );
 }
