d43eca9d949de501094cf5f49328085e28600643
dibyamgr <dibyamagar5@gmail.com>
2023-05-23 21:40:55 +0545

    React simple project setup

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
 import "bulma/css/bulma.css";
-import ProfileCard from "./ProfileCard";
-import AlexaImage from "./images/alexa.png";
-import CortanaImage from "./images/cortana.png";
-import SiriImage from "./images/siri.png";
 
 function App() {
   return (
     <>
       <section className="hero is-primary">
         <div className="hero-body">
-          <p className="title">Personal Digital Assistant</p>
+          <p className="title">React Simple Projects</p>
         </div>
       </section>
 
       <div className="container">
         <section className="section"></section>
         <div className="columns">
-          <div className="column is-4">
-            <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImage} description="Alexa was created by Amazon and helps you buy things." />
-          </div>
-          <div className="column is-4">
-            <ProfileCard title="Cortana" handle="@cortana99" image={CortanaImage} description="Cortana was created by Microsoft and helps you buy things." />
-          </div>
-          <div className="column is-4">
-            <ProfileCard title="Siri" handle="@siri99" image={SiriImage} description="Siri was created by Apple and being phased out." />
+          <div class="buttons">
+            <button class="button is-primary is-light">Personal Digital Assistant</button>
+            <button class="button is-link is-light">Amimal App</button>
           </div>
         </div>
       </div>
---
+++ src/Components/PDA.js
@@ -0,0 +1,48 @@
+import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+
+const PDA = () => {
+  return (
+    <>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistant</p>
+        </div>
+      </section>
+
+      <div className="container">
+        <section className="section"></section>
+        <div className="columns">
+          <div className="column is-4">
+            <ProfileCard
+              title="Alexa"
+              handle="@alexa99"
+              image={AlexaImage}
+              description="Alexa was created by Amazon and helps you buy things."
+            />
+          </div>
+          <div className="column is-4">
+            <ProfileCard
+              title="Cortana"
+              handle="@cortana99"
+              image={CortanaImage}
+              description="Cortana was created by Microsoft and helps you buy things."
+            />
+          </div>
+          <div className="column is-4">
+            <ProfileCard
+              title="Siri"
+              handle="@siri99"
+              image={SiriImage}
+              description="Siri was created by Apple and being phased out."
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PDA;
